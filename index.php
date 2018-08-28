4cf793f583625ea0e63e7bba56dec8e3ada4d43c
Marcelo Junior <rokermarcelo@gmail.com>
2018-08-27 21:06:52 -0300

    Criação do projeto NetBeans e metadados da página inicial

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="author" content="Marcelo Júnior">
+        <meta name="description" content="Sistema de gerenciamento de eventos e anexos do IFRN-SC">
+        <meta name="keywords" content="Santa Cruz, ifrn, evento, sistema">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>[SS] Sistema de Submissão</title>
+        <link rel="icon" href="img/logo250.png">
+    </head>
+    <body>
+        <h1>SS 2.0</h1>
+    </body>
+</html>
